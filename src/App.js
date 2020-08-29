b259cb99e7b18d480d7ac793b7dd561975967998
Sofia Kurochkina <sofikurochkina@gmail.com>
2020-08-29 22:10:30 +0300

    I love React

--- src/App.js
+++ src/App.js
@@ -1,18 +1,30 @@
 import React from "react";
 import "./App.css";
 import Header from "./Header";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <h1> Let's build a TINDER Clone App 🚀! </h1>
       {/* Header */}
-      <Header />
-      {/* Tinder cards */}
-      {/* Buttons below tinder cards */}
 
-      {/* Chats screen */}
-      {/* Individual chats screen */}
+      <Router>
+        <Header />
+        <Switch>
+          <Route path="/chat">
+            {" "}
+            <h1> I am the chatpage</h1>
+          </Route>
+          <Route path="/">
+            {" "}
+            <h1> I am homepage</h1>
+          </Route>
+        </Switch>{" "}
+        {/* Tinder cards */}
+        {/* Buttons below tinder cards */}
+        {/* Chats screen */}
+        {/* Individual chats screen */}
+      </Router>
     </div>
   );
 }
