b060cb98c80344288b58840114d0084da20c877b
Sofia Kurochkina <sofikurochkina@gmail.com>
2020-09-02 13:47:53 +0300

    building swipe buttons

--- src/SwipeButtons.js
+++ src/SwipeButtons.js
@@ -1,11 +1,37 @@
 import React from "react";
 
-function SwipeButtons(props) {
+import ReplayIcon from "@material-ui/icons/Replay";
+import CloseIcon from "@material-ui/icons/Close";
+import StarRateIcon from "@material-ui/icons/StarRate";
+import FavoriteIcon from "@material-ui/icons/Favorite";
+import FlashOnIcon from "@material-ui/icons/FlashOn";
+// make it a button
+import IconButton from "@material-ui/core/IconButton";
+
+import "./SwipeButtons.css";
+
+const SwipeButtons = () => {
   return (
-    <div>
-      <h2> Buttons go here</h2>
+    <div className="SwipeButtons">
+      <IconButton>
+        {" "}
+        <ReplayIcon fontSize="large" />
+      </IconButton>
+      <IconButton>
+        {" "}
+        <CloseIcon fontSize="large" />
+      </IconButton>
+      <IconButton>
+        <StarRateIcon fontSize="large" />
+      </IconButton>
+      <IconButton>
+        <FavoriteIcon fontSize="large" />
+      </IconButton>
+      <IconButton>
+        <FlashOnIcon fontSize="large" />
+      </IconButton>
     </div>
   );
-}
+};
 
 export default SwipeButtons;
